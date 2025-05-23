a22eadbace360bee4dbdc192baac9e5e48b05473
Lenadro Silva Lima <leandroslima09@gmail.com>
2025-05-23 18:39:46 +0000

    feat:solução da atividade

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,28 @@
-// Escreva seu código aqui!
+interface Sala {
+    TemperaturaSala: number;
+    Professora: string;
+    Luz: boolean;
+}
+
+let turma2: Sala = {
+    TemperaturaSala: 23,
+    Professora: 'Lana',
+    Luz: false,
+};
+
+console.log(turma2.TemperaturaSala)
+console.log(turma2.Professora)
+console.log(turma2.Luz)
+console.log(turma2.altura)
+
+let janelas: number = 4;
+let aluno: string = 'lucas';
+let arLigado: boolean = true;
+let QtdFios: undefined = undefined;
+
+console.log(janelas)
+console.log(aluno)
+console.log(arLigado)
+console.log(QtdFios)
+
+
